54a857b2e30b2d857cd48ae8eacfc1ec3b4a9d82
Lazar Savkovic <savkoviclazar1@gmail.com>
2022-08-31 22:54:01 +0200

    installed session connect flash and imported session into app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utilities/ExpressError")
+const session = require("express-session");
 
 const flats = require("./routes/flats")
 const apts = require("./routes/apts")
@@ -29,6 +30,21 @@ app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+
+
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+
+}
+app.use(session(sessionConfig))
 
 
 app.use("/flats", flats)
