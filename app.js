d9be08de723d829b9e1debc4898c19a62404f2b8
Lazar Savkovic <savkoviclazar1@gmail.com>
2022-09-02 19:19:44 +0200

    set up passport in the app

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utilities/ExpressError")
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local")
+const User = require("./models/user");
 
 const flats = require("./routes/flats")
 const apts = require("./routes/apts")
@@ -48,11 +51,20 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
+
+app.get("/fakeUse")
 
 
 app.use("/flats", flats)
