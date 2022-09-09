061326812b9eb811db77d1fbcec1725846e8dece
Lazar Savkovic <savkoviclazar1@gmail.com>
2022-09-09 15:59:14 +0200

    installed sanitize-html and added functionality to joi as extensioin

--- schemas.js
+++ schemas.js
@@ -1,9 +1,32 @@
-const Joi = require("joi");
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.aptSchema = Joi.object({
     apt: Joi.object({
-        title: Joi.string().required(),
-        short_description: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
+        short_description: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         floor: Joi.number().min(0),
         sq_mt: Joi.number().required().min(0),
@@ -15,9 +38,9 @@ module.exports.aptSchema = Joi.object({
 
 module.exports.flatSchema = Joi.object({
     flat: Joi.object({
-        title: Joi.string().required(),
-        short_description: Joi.string(),
-        location: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
+        short_description: Joi.string().escapeHTML(),
+        location: Joi.string().required().escapeHTML(),
         floor: Joi.number().min(0),
         sq_mt: Joi.number().required().min(0),
         rooms: Joi.number().required().min(0)
