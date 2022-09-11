33a9d70c828f13ff35c7d54df48e2301a58b192e
Lazar Savkovic <savkoviclazar1@gmail.com>
2022-09-11 22:32:53 +0200

    changed seeds to use dotenv functionality

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,17 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+
+
 const path = require('path');
 const mongoose = require("mongoose");
 const Apt = require("../models/apt");
 const apartments = require("./data_science/apartments.json")
 
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/procena-nekretnine";
 
-mongoose.connect("mongodb://localhost:27017/procena-nekretnine", {
+console.log(dbUrl)
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -14,6 +21,8 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
     console.log("Database connected")
 })
+
+
 
 const seedDB = async function () {
     await Apt.deleteMany({});
