343f417bc20a5fb550d02626d10d357fb59efaf5
Lazar Savkovic <savkoviclazar1@gmail.com>
2022-09-21 18:43:01 +0200

    added double slider price input for apts index page

--- public/javascripts/indexFilter.js
+++ public/javascripts/indexFilter.js
@@ -1,18 +1,24 @@
-const filterBar = document.getElementById("filter");
+const filterBar = document.getElementById("apts-name-filter");
+let cards = document.querySelectorAll(".card");
 
-filterBar.addEventListener("keyup", filterNames);
+filterBar.addEventListener("keyup", filterApts);
 
-function filterNames(){
-    let filterValue = document.getElementById("filter").value.toUpperCase();
-    let cardContainter = document.querySelector(".cards");
-    let cards = document.querySelectorAll(".card");
+rangeInput[0].addEventListener("change", filterApts);
+rangeInput[1].addEventListener("change", filterApts);
+
+function filterApts(){
+    let filterValue = document.getElementById("apts-name-filter").value.toUpperCase();
+
 for (let card of cards) {
     const title = card.children[0].children[1].children[0].children[0].innerHTML;
-    if (title.toUpperCase().indexOf(filterValue) > -1) {
+    const price = card.children[0].children[1].children[0].children[1].children[0].children[0].innerHTML;
+    const minPrice = parseInt(rangeInput[0].value);
+    const maxPrice = parseInt(rangeInput[1].value);
+    if ((title.toUpperCase().indexOf(filterValue) > -1) && ((price >= minPrice) && (price <= maxPrice)))
+     {
         card.style.display = "";
     } else {
         card.style.display = "none";
     }
 }
-
 }
